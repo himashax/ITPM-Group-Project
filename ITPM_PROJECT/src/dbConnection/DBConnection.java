0d90ae5d204f8754041ed1518b10f93189d04a5e
h_mandakini <himasha.mandakini@gmail.com>
2021-03-11 20:34:29 +0530

    initial commit

---
+++ ITPM_PROJECT/src/dbConnection/DBConnection.java
@@ -0,0 +1,22 @@
+package dbConnection;
+
+import java.sql.*;
+
+public class DBConnection {
+
+	public static Connection connect() {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ITPM", "root", "");
+			
+			if(connection != null) {
+				System.out.println("Successfully Connected");
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+}
