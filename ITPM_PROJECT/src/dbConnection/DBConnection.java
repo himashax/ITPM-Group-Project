4cb1eb8b01d7632a77e7202b05fb096afad18345
sandamaligeethika <sgeethika98@gmail.com>
2021-03-18 01:35:27 +0530

    Initial commit- member2

--- ITPM_PROJECT/src/dbConnection/DBConnection.java
+++ ITPM_PROJECT/src/dbConnection/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
 	public static Connection connect() {
 		Connection connection = null;
 		try {
-			connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/swing_demo", "root", "");
+			connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/project", "root", "");
 			
 			if(connection != null) {
 				System.out.println("Successfully Connected");
