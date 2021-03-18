cc7f3eb67d09865871287b364dc880e244064cf4
malakadasun <greenpagelocal7@gmail.com>
2021-03-18 21:50:41 +0530

    locations

--- ITPM_PROJECT/src/dbConnection/DBConnection.java
+++ ITPM_PROJECT/src/dbConnection/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
 	public static Connection connect() {
 		Connection connection = null;
 		try {
-			connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/swing_demo", "root", "");
+			connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/time_table", "root", "");
 			
 			if(connection != null) {
 				System.out.println("Successfully Connected");
@@ -18,5 +18,7 @@ public class DBConnection {
 		}
 		return connection;
 	}
-	
+	public static void main (String[] args ) {
+		connect();
+	}
 }
